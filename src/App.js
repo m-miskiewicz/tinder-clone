5f5f8942bee17aa280731c1767510705074070a1
Mateusz <youngbastardps4@gmail.com>
2020-12-21 21:44:43 +0100

    v1 commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ChatScreen from './ChatScreen';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Router>
         <Switch>
           <Route path="/chat/:person">
